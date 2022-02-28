85865fa9b5eab9e65277983cc7c0e9606197ea6e
Shaila Nasrin <drshaila2021@gmail.com>
2022-02-28 12:04:30 -0600

    third commit

--- js/phone.js
+++ js/phone.js
@@ -17,9 +17,37 @@ const getPhone = (searchName)=>{
 }
 const displayPhone=(phones)=>{
 console.log(phones);
+const displayPhoneContainer = document.getElementById("display-field-container");
+displayPhoneContainer.innerText='';
 phones.forEach(phone => {
+    
     const div=document.createElement("div");
+    div.classList.add('col')
+    div.innerHTML=`
+    <div class="card h-100">
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Name-${phone.phone_name}</h5>
+
+      <h5 class="card-title"> Brand-${phone.brand}</h5>
+
+     <button class="rounded btn btn-secondary" onclick="getDetail('${phone.slug}')" >DETAIL</button>
+    </div>
+  </div>
+    `
+displayPhoneContainer.appendChild(div)
 });
+
+}
+// get detail
+const getDetail=(phoneId)=>{
+    console.log(phoneId)
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
+    .then(res=>res.json())
+    .then(data=>console.log(data.data))
+}
+// display detail
+const displayDetail =(detailData)=>{
 
 }
 
